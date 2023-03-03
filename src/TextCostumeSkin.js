8e263bb78c656505029ce9584e5b06c3683c79c0
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2023-03-02 22:33:38 -0800

    Revert "Update TextCostumeSkin.js"
    This reverts commit 223d2a4063a0b0b98bc2b1cd302bdd5e6c360c33.

--- src/TextCostumeSkin.js
+++ src/TextCostumeSkin.js
@@ -81,7 +81,7 @@ class TextCostumeSkin extends Skin {
         this.style.STROKE_COLOR = textState.strokeColor;
         this.style.VERTICAL_PADDING = textState.size / 7;
         this.style.RAINBOW = textState.rainbow;
-        this._restyleCanvas()
+        this.measurementProvider.setFontAndSize(this.style.FONT, this.style.FONT_SIZE);
         this._textDirty = true;
         this._textureDirty = true;
         this.emit(Skin.Events.WasAltered);
